0684ca575481fe68bf5ee2907b3b1daeba62fc9e
Ramiro Rikkert <rrikkert@ripe.net>
2022-02-14 09:56:43 +0100

    Add some example JSON output fields.

--- src/main/java/net/ripencc/compo/Controller.java
+++ src/main/java/net/ripencc/compo/Controller.java
@@ -1,20 +1,37 @@
 package net.ripencc.compo;
 
 import net.ripencc.compo.dto.Hello;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicLong;
+
+import static java.time.LocalDateTime.now;
 
 @RestController
 public class Controller {
 
     private static final String template = "Hello, %s!";
+    private static final LocalDateTime bootTime = now();
     private final AtomicLong counter = new AtomicLong();
+
+    Logger logger = LoggerFactory.getLogger(Controller.class);
 
     @GetMapping("/greeting")
     public Hello greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Hello(counter.incrementAndGet(), String.format(template, name));
+        logger.info("greeting");
+
+        return Hello.builder()
+                .id(counter.incrementAndGet())
+                .duration(Duration.between(bootTime, now()))
+                .content(String.format(template, name))
+                .build();
     }
 }
--- src/main/java/net/ripencc/compo/dto/Hello.java
+++ src/main/java/net/ripencc/compo/dto/Hello.java
@@ -1,20 +1,23 @@
 package net.ripencc.compo.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Getter
 public class Hello {
 
     private final long id;
     private final String content;
+    private final List<String> RESULT = new ArrayList<>(0);
+    private final Duration duration;
 
-    public Hello(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
+    public ZonedDateTime getNow() {
+        return ZonedDateTime.now();
     }
 }
