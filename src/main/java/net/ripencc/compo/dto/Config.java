2b832f319a3fb335366d7b3821a72a05a3d8f948
Ramiro Rikkert <rrikkert@ripe.net>
2022-02-16 19:00:13 +0100

    Add API version.

--- src/main/java/net/ripencc/compo/dto/Config.java
+++ src/main/java/net/ripencc/compo/dto/Config.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Builder
 @Getter
 public class Config {
-    private String apiversion = "1";
+    private final String apiversion = "1";
     private String author;
     private String color;
     private String head;
