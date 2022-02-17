af920e3982937ed369a449baa46402fcb0f94bd3
Bas van der Hoek <bas@hoek.com>
2022-02-17 12:46:01 +0100

    imports

--- src/main/java/net/ripencc/compo/algo/RandomMove.java
+++ src/main/java/net/ripencc/compo/algo/RandomMove.java
@@ -1,22 +1,15 @@
 package net.ripencc.compo.algo;
 
 import net.ripencc.compo.dto.Battle;
-import net.ripencc.compo.dto.Board;
 import net.ripencc.compo.dto.Decision;
 import net.ripencc.compo.dto.Move;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.awt.*;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static net.ripencc.compo.dto.Move.Direction.left;
-import static net.ripencc.compo.dto.Move.Direction.up;
 
 @Component
 public class RandomMove {
