b8fae0d650f3d725eb3b3798ff5de02502d16efb
Ramiro Rikkert <rrikkert@ripe.net>
2022-02-17 12:41:55 +0100

    Add first game state decision.

--- src/main/java/net/ripencc/compo/algo/Strategy.java
+++ src/main/java/net/ripencc/compo/algo/Strategy.java
@@ -5,6 +5,7 @@ import net.ripencc.compo.dto.Battle;
 import net.ripencc.compo.dto.Board;
 import net.ripencc.compo.dto.Decision;
 import net.ripencc.compo.dto.Move;
+import net.ripencc.compo.dto.Snake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,12 @@ public class Strategy {
 
     private final Logger logger = LoggerFactory.getLogger(Strategy.class);
     private final Decision defaultDecision = Decision.builder().direction(left).build();
+    private enum State {
+        ANGRY,
+        HUNGRY,
+        FULL,
+        NORMAl
+    }
 
     private LegalMove legalMove;
     private RandomMove randomAlgo;
@@ -37,14 +44,35 @@ public class Strategy {
     }
 
     public Move determineNextMove(Battle battle) {
-        Decision decision = findFood.findClosest(battle)
+        Move.MoveBuilder result = Move.builder();
+        State state = getCurrentState(battle);
+
+        switch (state) {
+            case NORMAl:
+                result.direction(randomAlgo.getNextDirection(battle));
+                break;
+            case HUNGRY:
+                result.direction(findFood(battle).getDirection());
+                break;
+        }
+
+        return result.build();
+    }
+
+    private State getCurrentState(Battle battle) {
+        Snake you = battle.getYou();
+        if (you.getHealth() < 25) {
+            return State.HUNGRY;
+        }
+
+        return State.NORMAl;
+    }
+
+    private Decision findFood(Battle battle) {
+        return findFood.findClosest(battle)
                 .flatMap(p -> legalMove.getDirectionsTowardsPoint(battle, p))
                 .filter(Decision::isLegal)
                 .findFirst()
                 .orElse(defaultDecision);
-
-        return Move.builder()
-                .direction(decision.getDirection())
-                .build();
     }
 }
